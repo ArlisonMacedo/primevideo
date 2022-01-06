f90f0e07f42ee5afbd2b04987d4674f7ac956dd0
ArlisonMacedo <arlisonmacedo526@gmail.com>
2022-01-06 17:52:52 -0300

    finished menu end news components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import Header from '../../components/Header'
+import MovieItem from '../../components/MovieItem'
+
+import './styles.css'
 
 function Home () {
     return (
-        <Header />
+        <div className='home-page'>
+            <Header />
+            <div className='content'></div>
+            <div className='info-category'>
+                <img 
+                    className='prime-img-cat'
+                    src="https://m.media-amazon.com/images/G/01/digital/video/global/prime-logo-large-v4.png" 
+                    alt="prime video"
+                />
+                <p className='text-info'>Amazon Originals e exclusivos</p>
+            </div>
+            <MovieItem />
+        </div>
     )
 }
 
