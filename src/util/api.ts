0e39955a2af0dd0fdb37f569f88095aefc290b72
ArlisonMacedo <arlisonmacedo526@gmail.com>
2022-01-11 22:46:23 -0300

    finalizando top 10  e iniciando o consumo da api V 0.005

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,3 +1,6 @@
-import axios from 'axios'
+export const apiKey = 'api_key=cb60540d38bb4bcf11d206e2f682be31'
+export const apiBaseUrl = 'https://api.themoviedb.org/3/'
+export const language = 'language=pt-BR'
+export const imgOriginal = 'https://image.tmdb.org/t/p/original'
+export const imgResize = 'https://image.tmdb.org/t/p/w300'
 
-export default axios
